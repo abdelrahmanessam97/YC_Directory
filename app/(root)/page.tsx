0a6529e842528973e670fd12c32d562981af85a2
Abd Elrahman Essam Mohamed <bodyessam97a@gmail.com>
2025-03-07 11:48:11 +0200

    global styles

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,8 +7,8 @@ export default function Home() {
 
   return (
     <>
-      <section className="pink_container">
-        <h1 className="heading">
+      <section className="pink_container ">
+        <h1 className="heading ">
           Pitch Your Startup, <br />
           Connect With Entrepreneurs
         </h1>

5f89f0e56e58155279b0f0306d2ed08054f420fd
Abd Elrahman Essam Mohamed <bodyessam97a@gmail.com>
2025-03-08 13:42:50 +0200

    form component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,8 @@
-export default function Home() {
+import SearchForm from "@/components/SearchForm";
+
+export default async function Home({ searchParams }: { searchParams: Promise<{ query?: string }> }) {
+  const query = (await searchParams).query;
+
   // const data = await fetch("https://jsonplaceholder.typicode.com/posts").then((res) => res.json());
   // const data = await fetch("http://localhost:3000/api").then((res) => res.json());
   {
@@ -7,13 +11,14 @@ export default function Home() {
 
   return (
     <>
-      <section className="pink_container ">
+      <section className="pink_container">
         <h1 className="heading ">
           Pitch Your Startup, <br />
           Connect With Entrepreneurs
         </h1>
 
         <p className="sub-heading !max-w-3xl">Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.</p>
+        <SearchForm query={query} />
       </section>
 
       <section className="section_container"></section>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,13 +4,12 @@ import Link from "next/link";
 
 const Navbar = async () => {
   const session = await auth();
-  console.log(session);
 
   return (
     <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
       <nav className="flex justify-between items-center">
         <Link href="/">
-          <Image src="/logo.png" alt="Logo" width={144} height={30} priority />
+          <Image src="/logo.png" alt="Logo" width={144} height={30} />
         </Link>
         <div className="flex items-center gap-5 text-black">
           {session && session?.user ? (
